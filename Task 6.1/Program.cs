c64e4121fbb1c66bea27cd70b954eccd0d2c5f6b
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-13 15:58:48 +0300

    .

--- Task 6.1/Program.cs
+++ Task 6.1/Program.cs
@@ -10,6 +10,31 @@ namespace Task_6._1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Введите размерность массива:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] mass = new int[n];
+                Random r = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    mass[i] = r.Next(-50, 50);
+                    Console.Write(mass[i] + " ");
+                }
+                Console.WriteLine();
+                for (int i = 1; i < n; i = i + 2)
+                {
+                    if (mass[i] > 0)
+                    {
+                        Console.Write(mass[i] + " ");
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
